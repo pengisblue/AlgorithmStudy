d498ea331453459638f21c223ca68289503d881b
정지헌 <hun01414@naver.com>
2023-07-26 08:47:27 +0900

    지헌주석추가

--- 0x04/1158_지헌.py
+++ 0x04/1158_지헌.py
@@ -9,6 +9,7 @@ N, K = map(int, input().split())
 head = Node(1)
 node = head
 
+# 주어진 N 길이로 Node 를 연결
 for i in range(1, N):
     while node:
         if node.next is None:
@@ -18,6 +19,7 @@ for i in range(1, N):
 
 node = head
 
+# N 번째 Node 와 head를 연결하여 순환리스트 형성
 while node:
     if node.next is None:
         node.next = head
@@ -26,6 +28,8 @@ while node:
 
 ls = []
 
+# K-1 번째 노드의 next값을 print
+# next.next 값을 연결하여 node.next 값을 연결에서 탈락
 for i in range(N):
     for j in range(K-1):
         node = node.next
